3a8ded18e7fe8174d7b5dbd636966c6436c1a9be
Garance LAMBEY <galambey@f2r11s20.clusters.42paris.fr>
2025-12-23 12:50:35 +0100

    add docker / kubectl / k3d installation + GIT CLI working

--- vm_init.sh
+++ vm_init.sh
@@ -18,66 +18,58 @@ useradd -m -p $PASS $1
 
 apt-get update && apt-get upgrade
 
-# SSH
-echo "*************SSH************"
-# Create and manage owner and rights on .ssh directory
-mkdir -p /home/$1/.ssh
-chown -R $1:$1 /home/$1/.ssh
-chmod 700 /home/$1/.ssh
-# Generate ssh key
-ssh-keygen -t rsa -b 4096 -C $4 -f /home/$1/.ssh/id_rsa -N ""
-# Manage owner and rights on id_rsa files
-chown -R $1:$1 /home/$1/.ssh/id_rsa
-chown -R $1:$1 /home/$1/.ssh/id_rsa.pub
-chmod 600 /home/$1/.ssh/id_rsa
-chmod 644 /home/$1/.ssh/id_rsa.pub
-# ssh-add /home/$1/.ssh/id_rsa
+# # SSH
+# echo "*************SSH************"
+# # Create and manage owner and rights on .ssh directory
+# mkdir -p /home/$1/.ssh
+# chown -R $1:$1 /home/$1/.ssh
+# chmod 700 /home/$1/.ssh
+# # Generate ssh key
+# ssh-keygen -t rsa -b 4096 -C $4 -f /home/$1/.ssh/id_rsa -N ""
+# # Manage owner and rights on id_rsa files
+# chown -R $1:$1 /home/$1/.ssh/id_rsa
+# chown -R $1:$1 /home/$1/.ssh/id_rsa.pub
+# chmod 600 /home/$1/.ssh/id_rsa
+# chmod 644 /home/$1/.ssh/id_rsa.pub
+# # ssh-add /home/$1/.ssh/id_rsa
 
 # Git"
 echo "GIT"
 apt-get install git -y
-# GIT CLI INSTALLATION
-apt-get install gh
-# # Authentificate to github account through classic token
+# # GIT CLI INSTALLATION
+# apt-get install gh
+# # # Authentificate to github account through classic token
 # echo $3 > token.txt
 # gh auth login --with-token < token.txt
-# # Adding ssh_key to github account
+# # # Adding ssh_key to github account
 # gh ssh-key add /home/$1/.ssh/id_rsa.pub --title "IOT"
-# # To be able to add, commit, push
-# # echo "$(whoami)"
-# # echo "********************TEST0**************************"
-# # sudo su - $1 << $2
-# # echo "$(whoami)"
-# # echo "********************TEST1**************************"
-# # git config --global user.email "$4"
-# # echo "********************TEST2**************************"
-# # git config --global user.name "$5"
-# # echo "********************TEST3**************************"
-# # exit
-# # echo "$(whoami)"
+# # # To be able to add, commit, push => config avec l utilisateur
+# sudo -u $1 git config --global user.email "$4"
+# sudo -u $1 git config --global user.name "$5"
+# rm token.txt
 
 # To force user to change his passwd at first connexion
 passwd -e $1
 # To give sudo rigths
 usermod -aG sudo $1
 
-# Make
-echo "MAKE"
-apt-get install make -y
+# # Make
+# echo "MAKE"
+# apt-get install make -y
 
-# Install wget and gpg
-echo "WGET GPG"
-apt-get install wget gpg -y
+# # Install wget and gpg
+# echo "WGET GPG"
+# apt-get install wget gpg -y
 
-# Vscode
-echo "VSCODE"
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
-apt-get install apt-transport-https
-apt-get update
-apt-get install code
+# # Vscode
+# echo "VSCODE"
+# wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+# install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+# echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+# rm -f packages.microsoft.gpg
+# apt-get install apt-transport-https
+# apt-get update
+# apt-get install code
 
 # # Vagrant
 # echo "VAGRANT"
@@ -100,30 +92,48 @@ apt-get install code
 ## TO DO :
 # - Add modification file sudoers to allow a certain member to execute through a script sudo
 
-# Docker
-apt remove $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1)
-# Add Docker's official GPG key:
-apt update
-apt install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+# # Docker
+# apt-get remove $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1)
+# # Add Docker's official GPG key:
+# apt-get update
+# apt-get install ca-certificates curl
+# install -m 0755 -d /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+# chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/debian
-Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
-Components: stable
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
+# # Add the repository to Apt sources:
+# tee /etc/apt/sources.list.d/docker.sources <<EOF
+# Types: deb
+# URIs: https://download.docker.com/linux/debian
+# Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+# Components: stable
+# Signed-By: /etc/apt/keyrings/docker.asc
+# EOF
 
-apt update
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# apt-get update
+# apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# # To check installation :
+# sudo docker run hello-world
 
-# Install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# # Install kubectl
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+# install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# # To check installation :
+# kubectl version --client
 
-#Install K3D
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+# # Install K3D
+# wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+# # To check installation :
+# k3d --help
+
+# Erase Vagrant traces => A TESTER
+umount /vagrant 2>/dev/null || true
+userdel -r vagrant 2>/dev/null || true # 2>/dev/null || true > avoid to stop script before reboot in case user doesn't exist 
+rm -f /etc/ssh/sshd_config.d/*vagrant* 2>/dev/null || true
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+history -c
+cat /dev/null > /root/.bash_history
+cat /dev/null > /home/$1/.bash_history 2>/dev/null || true
+
 reboot
